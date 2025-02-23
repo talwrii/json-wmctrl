4d234d320f007d1cbe8f782563ed32765f316e02
Tal Wrii <talwrii@gmail.com>
2025-02-23 20:28:43 +0100

    Set up version info

--- setup.py
+++ setup.py
@@ -3,21 +3,18 @@ import distutils.core
 
 setuptools.setup(
     name='json-wmctrl',
-    version=0.1,
-    author='Author',
+    version="1.0.0",
+    author='@readwithai',
     long_description_content_type='text/markdown',
-    author_email='Email',
-    description='',
+    author_email='talwrii@gmail.com',
+    description='List X windows with json output',
     license='MIT',
-    keywords='',
-    url='',
+    keywords='X, wmctrl, window manager',
+    url='https://github.com/talwrii/json-wmctrl',
     packages=["json_wmctrl"],
     long_description=open('README.md').read(),
     install_requires=["Xlib"],
     entry_points={
         'console_scripts': ['json-wmctrl=json_wmctrl.main:main']
     },
-    classifiers=[
-    ],
-    test_suite='nose.collector'
 )
